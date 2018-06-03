979b51b56364b894da5b3919c2ddf2213204da08
Jeff O'Connell <jeffrey.oconnell@gmail.com>
2018-06-03 06:05:32 -0700

    Update cash.py

--- pset6/cash/cash.py
+++ pset6/cash/cash.py
@@ -1,1 +1,27 @@
-here
+import cs50
+
+def main():
+    while True:
+        #get the amount of change due from the user
+        print("Change owed: ", end = "")
+        money = cs50.get_float()
+        if money>=0:
+            break
+
+    #covert the money (in decimal) to cents (an integer)
+    cents = int(money*100)
+
+    #find the number of quarters
+    quarters = cents // 25
+    #subtract the value of the quarters and find the number of dimes
+    dimes = (cents - 25*quarters) // 10
+    #do it again for nickels
+    nickels = (cents - 25*quarters - 10*dimes) // 5
+    #the rest are pennies
+    pennies = cents - 25*quarters - 10*dimes - 5*nickels
+
+    #print the result :-)
+    print("{}".format(quarters+dimes+nickels+pennies))
+
+if __name__ == "__main__":
+    main()
